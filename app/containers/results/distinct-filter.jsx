8293c82274976d8466723075f325acf2598eae24
Khalid Husain <khusain444@gmail.com>
2019-01-06 18:46:38 -0500

    refactor DistinctFilter to Dropdown

--- app/containers/results/distinct-filter.jsx
+++ app/containers/results/distinct-filter.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 
 const DistinctFilter = ({ name, options }) => {
     return (
-        <Form>
-            {name}
-
+        // <Form>
+        //     {name}
+        //     {Object.keys(options).map(key => {
+        //         return <Form.Check key={key} type='checkbox' id={key} label={key} />;
+        //     })}
+        // </Form>
+        <DropdownButton title={name}>
             {Object.keys(options).map(key => {
-                return <Form.Check key={key} type='checkbox' id={key} label={key} isValid='true'/>;
-            })}
-        </Form>
+                return <Dropdown.Item key={key} disabled={options[key]}>{key}</Dropdown.Item>
+            })}  
+        </DropdownButton>
     );
 };
 
