b8854013e600d63f4d3f9c7b5c4605f15e855015
Khalid Husain <khusain444@gmail.com>
2019-01-05 19:34:53 -0500

    Add thunk to toggle state.isLoading in redux store

--- app/store/index.js
+++ app/store/index.js
@@ -4,6 +4,7 @@ import loggingMiddleware from 'redux-logger'
 import axios from 'axios';
 
 const GOT_RESULTS = "GOT_RESULTS";
+const TOGGLE_LOADING = "TOGGLE_LOADING";
 
 const initialState = {
     searchURL: '',
@@ -12,6 +13,12 @@ const initialState = {
     isLoading: false,
     currentBook:{}
 };
+
+const toggleLoading = () => {
+    return {
+        type: TOGGLE_LOADING
+    }
+;}
 
 const gotSearchResults = (results, searchURL, currentBook) => {
     return {
@@ -25,7 +32,9 @@ const gotSearchResults = (results, searchURL, currentBook) => {
 export const queryAPI = (queryPrefix, queryBody) => {
     return async dispatch => {
         const formattedQueryBody = queryBody.split(' ').join('+');
+        dispatch(toggleLoading());
         const response = await axios.get(`http://openlibrary.org/search.json?${queryPrefix}=${formattedQueryBody}&limit=1000`);
+        dispatch(toggleLoading());
         const results = response.data.docs;
         const searchURL = `?${queryPrefix}=${formattedQueryBody}`
         const action = gotSearchResults(results, searchURL, results[0]);
@@ -38,6 +47,8 @@ const reducer = (state = initialState, action) => {
         case GOT_RESULTS:
             const { results, searchURL, currentBook } = action;
             return { ...state, results , filteredResults: results, searchURL, currentBook};
+        case TOGGLE_LOADING:
+            return { ...state, isLoading: !state.isLoading};
         default:
             return state;
     };
