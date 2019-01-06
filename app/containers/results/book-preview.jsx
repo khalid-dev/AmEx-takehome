52a124dd62f5fa8b347d99f8813c5617f0b31083
Khalid Husain <khusain444@gmail.com>
2019-01-06 14:08:07 -0500

    Complete BookPreview Component

--- app/containers/results/book-preview.jsx
+++ app/containers/results/book-preview.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Image } from 'react-bootstrap';
 import { withRouter } from "react-router";
 
 class BookPreview extends Component {
     render () {
-        console.log(this.props);
+        const { bookInfo, bookIx, history } = this.props;
+        const { cover_i, title, author_name, first_publish_year } = bookInfo;
+        const coverURL = cover_i ? `http://covers.openlibrary.org/b/id/${cover_i}-M.jpg` : `https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg`;
         return (
-            <Row>
+            <Row onClick={() => history.push(`${history.location.pathname}/${bookIx}`)}>
                 <Col>
+                    <Image src={coverURL} thumbnail/>
                 </Col>
                 <Col>
                     <Row>
-
+                        Title: {title}
                     </Row>
                     <Row>
-                        
+                        Author: {author_name || 'No author specified'}
                     </Row>
                     <Row>
-                        
+                        First Published In: {first_publish_year || 'No publish date specified'}
                     </Row>
                 </Col>
             </Row>
