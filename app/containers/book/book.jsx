a0cc3f01f80d651d639e9842328e5af382dde196
Khalid Husain <khusain444@gmail.com>
2019-01-09 00:17:41 -0500

    Styled Book Component

--- app/containers/book/book.jsx
+++ app/containers/book/book.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Container } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
 import BookInfo from './book-info.jsx';
 import { SearchBar } from '../../components/index.js';
 
 class Book extends Component {
     render() {
         const { filteredResults, match } = this.props;
+        const book = filteredResults[match.params.bookId];
         return (
             <Container>
                 <SearchBar />
-                <BookInfo book={filteredResults[match.params.bookId]}/>
+                <Card>
+                    <Card.Header className="text-center">{`${book.title}, ${book.author_name}`}</Card.Header>
+                    <BookInfo book={book}/>
+                </Card>
             </Container>
         );
     };
