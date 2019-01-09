9247ef42bdc1e48120d4e1cb755125c1e7ed0160
Khalid Husain <khusain444@gmail.com>
2019-01-08 23:43:18 -0500

    Implement minor util function to normalize names displayed on filters

--- app/containers/results/distinct-filter.jsx
+++ app/containers/results/distinct-filter.jsx
@@ -4,9 +4,9 @@ import { Dropdown, DropdownButton } from 'react-bootstrap';
 const DistinctFilter = ({ name, options, setFilter, applyFilters, toggleAllFilters }) => {
     const optionsCopy = Object.keys(options).slice();
     optionsCopy.sort();
+    const filterName = name.split('_').map(word => (word.charAt(0).toUpperCase() + word.slice(1))).join(' ');
     return (
-        //style={{padding: '.2em'}}
-        <DropdownButton title={name} className="customDropdown">
+        <DropdownButton title={filterName} className="customDropdown">
             <Dropdown.Item active={false} onClick={() => toggleAllFilters(true)}>Select All</Dropdown.Item>
             <Dropdown.Item active={false} onClick={() => toggleAllFilters(false)}>Unselect All</Dropdown.Item>
             {optionsCopy.map(key => {
