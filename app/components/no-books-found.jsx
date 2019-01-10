bae44131938b39bd9e80f74f80ab63244010b3bb
Khalid Husain <khusain444@gmail.com>
2019-01-09 22:57:44 -0500

    Style error

--- app/components/no-books-found.jsx
+++ app/components/no-books-found.jsx
@@ -3,8 +3,8 @@ import { Alert } from 'react-bootstrap';
 
 const noBooksFound = () => {
     return (
-        <Alert variant="danger">
-            <Alert.Heading>There are no books to look at! :(</Alert.Heading>
+        <Alert variant="danger" className="text-center">
+            <Alert.Heading>There are no books to look at :(</Alert.Heading>
             <p>
                 Sorry for the inconvenience! Please perform a new search.
             </p>
